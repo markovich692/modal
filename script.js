edc3b13a9aea58a566bcc9dd04aaa727ec52346c
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-04 17:56:49 -0600

    adds the click event on the close-modal button

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ for (let i = 0; i < showModal.length; i++) {
   });
 }
 
+closeModal.addEventListener('click', function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+});
+
 // for (let i = 0; i < showModal.length; i++) {
 //   showModal[i].addEventListener('click', function () {
 //     console.log(showModal[i]);
